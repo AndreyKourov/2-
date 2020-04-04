f8347671837311b2169245d2cc25775e5a5c52a1
AndreyKourov <andreykourov@mail.ru>
2020-04-04 15:01:21 +0600

    первое

---
+++ LessonTwo/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp2';
+}
+//list-products
+//view-products
+
+//Angular 8 and TypeScript/HTML VS Code Snippets
+//Angular Language Service
+//Angular Files
---
+++ LessonTwo/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ListProductsComponent,
+      ViewProductsComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
